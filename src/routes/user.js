abc2f3e6adc7d7090d6deacc1cf715c4eed147b3
shiina <ben410332@gmail.com>
2021-03-25 19:58:03 +0800

    add api function

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,38 +1,24 @@
 import {Router} from 'express';
 import controller from '../controllers/user.js'
 
-// class UserController{
-//     getuser = (req, res)=>{
-//         const name = "Jack";
-//         const {token} = req.query;
-//         res.status(200).json({message:`${token}.${name}`});
-//     }
-
-//     postuser = (req, res)=>{
-//         const {body} = req;
-//         const {name, phone} = body;
-//         const profile = {
-//             name,
-//             phone,
-//         }
-//         res.status(200).json({profile});
-//         console.log(body);
-//         console.log(req);
-//     } 
-// }
-
-// const controller = new UserController;
-
 const router = Router();
 
 // router.use('/', (req,res)=>{
 //     res.status(200).json({message:'Hi'});
 // });
 
-router.get('/search', controller.getuser);
-router.post('/add', controller.postuser);
-router.post('/destory', controller.destroyuser);
-router.post('/update', controller.updateuser);
+router.get('/searchUser', controller.getuser);
+router.post('/addUser', controller.postuser);
+router.post('/destoryUser', controller.destroyuser);
+router.post('/updateUser', controller.updateuser);
+
+//or
+/*
+router.get('/', controller.getuser);
+router.post('/', controller.postuser);
+router.delete('/', controller.destroyuser);
+router.patch('/', controller.updateuser);
+*/
+
 
 export default router;
-
